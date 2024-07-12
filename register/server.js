ddf7522b291d345909f9e4029ebd84c7383b2bdb
Akshara Santhosh <aksharasanthosh3003@gmail.com>
2024-07-13 00:15:48 +0530

    updated

--- register/server.js
+++ register/server.js
@@ -1,47 +1,50 @@
- const express = require('express')
- const mongoose = require('mongoose')
- const path = require('path')
- const port = 3019
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const port = 3019;
 
- const app = express();
- app.use(express.static(__dirname))
- app.use(express.urlencoded({extended :true}))
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
 
- mongoose.connect('mongodb://localhost:27017/Users')
- const db = mongoose.connection
- db.once('open',()=>{
-    console.log("Mongodb connection successful")
- })
+mongoose.connect('mongodb://localhost:27017/Users');
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log("Mongodb connection successful");
+});
 
- const userSchema = new mongoose.Schema({
-    regd_no:String,
-    mobile:Number,
-    email:String,
-    name:String
- })
+const userSchema = new mongoose.Schema({
+    regd_no: String,
+    mobile: Number,
+    email: String,
+    name: String
+});
 
- const Users = mongoose.model("data",userSchema)
+const Users = mongoose.model("data", userSchema);
 
- app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'form.html'))
- })
- 
- app.post('/post',async(req,res)=>{
-    const {regd_no,mobile, email,name} = req.body
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'form.html'));
+});
+
+app.post('/post', async (req, res) => {
+    const { regd_no, mobile, email, name } = req.body;
     const user = new Users({
         regd_no,
         mobile,
         email,
         name
-    })
-    await user.save()
-    console.log(user)
-   ' res.send("Form Submitted Suceessfully")'
-   res.redirect('/')
-    
+    });
 
- })
+    try {
+        await user.save();
+        console.log(user);
+        res.send('<script>alert("Form Submitted Successfully"); window.location="/";</script>');
+    } catch (error) {
+        console.error(error);
+        res.send('<script>alert("Error submitting form. Please try again later."); window.location="/";</script>');
+    }
+});
 
- app.listen(port,()=>{
-    console.log("Server started")
- })
+app.listen(port, () => {
+    console.log("Server started");
+});
